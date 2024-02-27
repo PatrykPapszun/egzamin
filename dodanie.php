bd75b3078b22e2973860bf22cef11cbd4fbe6fa7
Patryk Papszun <papszunpatryk@gmail.com>
2024-02-27 13:04:24 +0100

    koniec?

--- dodanie.php
+++ dodanie.php
@@ -1,22 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-        $con = mysqli_connect('localhost', 'root', '', 'ee09');
-        @$nrKaretki = $_POST['nrKaretki'];
-        @$ratownik1 = $_POST['ratownik1'];
-        @$ratownik2 = $_POST['ratownik2'];
-        @$ratownik3 = $_POST['ratownik3'];
-        $zap = mysqli_query($con, "INSERT INTO ratownicy('nrKaretki', 'ratownik1', 'ratownik2', 'ratownik3')");
-        
-        while($option = mysqli_fetch_array($zap)){
-
-        }
-    ?>
-</body>
-</html>
+<?php
+ 
+$conn = mysqli_connect("localhost", "root", "", "ee09");
+ 
+$nrKaretki = $_POST['nrKaretki'];
+$imie1 = $_POST['imie1'];
+$imie2 = $_POST['imie2'];
+$imie3 = $_POST['imie3'];
+ 
+ 
+$sql = "INSERT INTO `ratownicy`(`nrKaretki`, `ratownik1`, `ratownik2`, `ratownik3`) VALUES ($nrKaretki, '$imie1', '$imie2', '$imie3')";
+$zapytanie = mysqli_query($conn,$sql);
+echo "Do bazy zostało wysłane zapytanie: ".$sql;
+mysqli_close($conn);
+?>
