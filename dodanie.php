937c424a5caee0faf9f18525df494dc6806daf76
Patryk Papszun <papszunpatryk@gmail.com>
2024-03-05 11:47:17 +0100

    PROSZE ZADZIALAJ

--- dodanie.php
+++ dodanie.php
@@ -1,15 +1,14 @@
 <?php
- 
-$conn = mysqli_connect("localhost", "root", "", "ee09");
- 
-$nrKaretki = $_POST['nrKaretki'];
-$imie1 = $_POST['imie1'];
-$imie2 = $_POST['imie2'];
-$imie3 = $_POST['imie3'];
- 
- 
-$sql = "INSERT INTO `ratownicy`(`nrKaretki`, `ratownik1`, `ratownik2`, `ratownik3`) VALUES ($nrKaretki, '$imie1', '$imie2', '$imie3')";
-$zapytanie = mysqli_query($conn,$sql);
-echo "Do bazy zostało wysłane zapytanie: ".$sql;
-mysqli_close($conn);
+    $conn = mysqli_connect("localhost", "root", "", "ee09");
+    
+    $nrKaretki = $_POST['nrKaretki'];
+    $imie1 = $_POST['ratownik1'];
+    $imie2 = $_POST['ratownik2'];
+    $imie3 = $_POST['ratownik3'];
+    
+    
+    $sql = "INSERT INTO `ratownicy`(`nrKaretki`, `ratownik1`, `ratownik2`, `ratownik3`) VALUES ($nrKaretki, '$imie1', '$imie2', '$imie3')";
+    $zapytanie = mysqli_query($conn, $sql);
+    echo "Do bazy zostało wysłane zapytanie: ".$sql;
+    mysqli_close($conn);
 ?>
